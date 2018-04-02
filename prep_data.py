aa19b8aaf34c28ac81d59fd0b621cf115f4e75ab
Spencer Smith <spencer.smith2012@gmail.com>
2018-04-02 15:00:55 -0600

    Prep data now uses panadas' dataframes to gather image paths and ids

--- prep_data.py
+++ prep_data.py
@@ -2,28 +2,37 @@ import sys
 import pandas as pd
 import numpy as np
 from glob import glob
-import cv2
+import matplotlib.pyplot as plt
+#import cv2
 import os
 from PIL import Image
 
 
 def ImportImage(filename):
-    img = cv2.imread(filename)
+    # img = cv2.imread(filename)
+    print(filename);
+    img = plt.imread(filename);
     return img
 
 def training_data(training_sets):
-    training_images = []
-    image_label_dict = {}
+    # Initialize empty dataframe
+    df = None;
     for training_set in training_sets:
-        training_images = training_images + glob(f'{training_set}/train/*jpg')
-        df = pd.read_csv(f'{training_set}/train.csv')
-        df["Image"] = df["Image"].map( lambda x : f'{training_set}/train/{x}')
-        image_label_dict = { **image_label_dict, **dict( zip( df["Image"], df["Id"])) }
-
-
-
-    train_imgs = np.array([ImportImage( img) for img in training_images])
-    train_labels = list(map(image_label_dict.get, training_images))
-    return {'train_images': train_imgs, 'train_labels': np.array(train_labels)}
+      # Remove ending /
+      if training_set[-1] == "/":
+        training_set = training_set[:-1];
+      # Append train.csv for each set into the dataframe
+      train = pd.read_csv(training_set + '/train.csv');
+      train['Image'] = (training_set + '/train/') + train['Image'].astype(str);
+      if df:
+        df = df.append(train);
+      else:
+        df = train;
+    
+    image_paths = df['Image'].tolist();
+    print(image_paths);
+    train_imgs = np.array([ImportImage(path) for path in image_paths])
+    train_labels = np.array(df['Id'].tolist());
+    return {'train_images': train_imgs, 'train_labels': train_labels}
 
 training_data(sys.argv[1:])
