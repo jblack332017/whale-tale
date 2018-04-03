869781de5066e43e1214fd11892a6397bcf52669
Josh Black <josh.m.black.work@gmail.com>
2018-04-02 22:18:05 -0600

    python2 ify

--- prep_data.py
+++ prep_data.py
@@ -13,6 +13,11 @@ def ImportImage(filename):
     img = cv2.resize(img ,(int(SIZE),int(SIZE)))
     return img
 
+def merge_two_dicts(x, y):
+    z = x.copy()   # start with x's keys and values
+    z.update(y)    # modifies z with y's keys and values & returns None
+    return z
+
 def training_data(training_sets):
     training_images = []
     image_label_dict = {}
@@ -20,8 +25,7 @@ def training_data(training_sets):
         training_images = training_images + glob(training_set + '/train/*jpg')
         df = pd.read_csv(training_set + '/train.csv')
         df["Image"] = df["Image"].map( lambda x : training_set+ '/train/' + x)
-        image_label_dict = { **image_label_dict, **dict( zip( df["Image"], df["Id"])) }
-
+        image_label_dict = merge_two_dicts(image_label_dict, dict( zip( df["Image"], df["Id"])))
 
 
     train_imgs = np.array([ImportImage( img) for img in training_images])
