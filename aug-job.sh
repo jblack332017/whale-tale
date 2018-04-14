b5237870abf09e4b841845c9b8679beec535dce6
Josh <josh.m.black@hotmail.com>
2018-04-13 18:16:08 -0600

    add aug-job.sh

--- aug-job.sh
+++ aug-job.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#SBATCH --time=03:00:00   # walltime
+#SBATCH --time=12:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --gres=gpu:1
-#SBATCH --mem-per-cpu=32768M   # memory per CPU core
-#SBATCH -J "large-aug-1"   # job name
+#SBATCH --mem-per-cpu=65536M   # memory per CPU core
+#SBATCH -J "aug-large"   # job name
 #SBATCH --mail-user=josh.m.black.work@gmail.com   # email address
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
+
 
 module load python/2/7
 module load cuda/8.0
