994e97810bef8a48b0dd3875985e0661f86ed48d
Josh Black <josh.m.black.work@gmail.com>
2018-04-18 12:04:29 -0600

    cleaning up unused files

--- testmodel.sh
+++ testmodel.sh
@@ -1,8 +1,8 @@
-#python whale-tale.py insub.csv input > input.log
-#python whale-tale.py augcolsub.csv input augmented-correct-colors > augcol.log
-#python whale-tale.py auggraysub.csv input augmented-correct-gray > auggray.log
+python whale-tale.py insub.csv input > input.log
+python whale-tale.py augcolsub.csv input augmented-correct-colors > augcol.log
+python whale-tale.py auggraysub.csv input augmented-correct-gray > auggray.log
 python whale-tale.py augrotsub-no-.csv input-sans-new-whale augmented-correct-rotation > augrot-no-new-whale.log
-#python whale-tale.py augshearsub.csv input augmented-correct-shear > augshear.log
-#python whale-tale.py augzoomsub.csv input augmented-correct-zoom > augzoom.log
-#python whale-tale.py auglargesub.csv input augmented-correct-large > auglarge.log
+python whale-tale.py augshearsub.csv input augmented-correct-shear > augshear.log
+python whale-tale.py augzoomsub.csv input augmented-correct-zoom > augzoom.log
+python whale-tale.py auglargesub.csv input augmented-correct-large > auglarge.log
 
